dc0f43c7a9a2fb491fcede0394851688f7bc0572
Lai-Mei Ng <lng@opentable.com>
2017-06-08 16:37:51 -0700

    Add link as runbook

--- main.rb
+++ main.rb
@@ -18,6 +18,7 @@
       :environment => env,
       :team        => v['Team'],
       :status      => (proj.get_status == "FAILURE") ? 2 : 0,
+      :runbook     => "#{proj.get_tc_link}",
       :output      => "Status: #{proj.get_status}\\n" \
                       "ProjectName: #{proj.get_tc_project_name}\\n" \
                       "TestName: #{proj.get_test_name}\\n" \
@@ -26,8 +27,7 @@
                       "LastRun: #{last_run}\\n" \
                       "LastRunTimestamp: #{proj.get_last_run}\\n" \
                       "GreenSince: #{last_green}\\n" \
-                      "GreenSinceTimestamp: #{proj.get_last_success_run}\\n" \
-                      "TeamCityLink: ['#{proj.get_tc_link}']"
+                      "GreenSinceTimestamp: #{proj.get_last_success_run}"
     }
     puts JSON.generate(sensu_obj)
   }
