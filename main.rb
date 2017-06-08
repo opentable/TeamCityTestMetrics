40317ea0a3812025cdd1ac9f2441d1542224134a
Lai-Mei Ng <lng@opentable.com>
2017-06-08 11:54:27 -0700

    Try with just a simple quote

--- main.rb
+++ main.rb
@@ -11,23 +11,23 @@
   test_hash.each { | k, v |
     #TeamCity project object for a test suite
     proj = TCProjects.new(v['TeamCityHost'], v['Project'], v['Test'], env)
-    last_run = Utilities.get_lapse_time(proj.get_last_run, DateTime.now.to_s).gsub(' ','%20')
-    last_green = Utilities.get_lapse_time(proj.get_last_success_run, DateTime.now.to_s).gsub(' ','%20')
+    last_run = Utilities.get_lapse_time(proj.get_last_run, DateTime.now.to_s)
+    last_green = Utilities.get_lapse_time(proj.get_last_success_run, DateTime.now.to_s)
     sensu_obj = {
       :name        => "CW_Auto_Metrics_#{k}",
       :environment => env,
       :team        => v['Team'],
       :status      => (proj.get_status == "FAILURE") ? 2 : 0,
-      :output      => "Status:%20#{proj.get_status}%0A" \
-                      "ProjectName:%20#{proj.get_tc_project_name}%0A" \
-                      "TestName:%20#{proj.get_test_name}%0A" \
-                      "TestFailed:%20#{proj.get_test_failed}%0A" \
-                      "TotalTests:%20#{proj.get_total_tests}%0A" \
-                      "LastRun:%20#{last_run}%0A" \
-                      "LastRunTimestamp:%20#{proj.get_last_run}%0A" \
-                      "GreenSince:%20#{last_green}%0A" \
-                      "GreenSinceTimestamp:%20#{proj.get_last_success_run}%0A" \
-                      "TeamCityLink:%20'#{proj.get_tc_link}'"
+      :output      => "Status: #{proj.get_status}\\n" \
+                      "ProjectName: #{proj.get_tc_project_name}\\n" \
+                      "TestName: #{proj.get_test_name}\\n" \
+                      "TestFailed: #{proj.get_test_failed}\\n" \
+                      "TotalTests: #{proj.get_total_tests}\\n" \
+                      "LastRun: #{last_run}\\n" \
+                      "LastRunTimestamp: #{proj.get_last_run}\\n" \
+                      "GreenSince: #{last_green}\\n" \
+                      "GreenSinceTimestamp: #{proj.get_last_success_run}\\n" \
+                      "TeamCityLink: '#{proj.get_tc_link}'"
     }
     puts JSON.generate(sensu_obj)
   }
